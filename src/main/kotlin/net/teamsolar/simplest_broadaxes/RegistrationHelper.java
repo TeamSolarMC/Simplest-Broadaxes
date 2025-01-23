05512cb627cbbf3ac202d314ba760018a0794296
xeli <sheeteditor2@gmail.com>
2025-01-22 20:44:40 -0500

    cleanup unused files

--- src/main/kotlin/net/teamsolar/simplest_broadaxes/RegistrationHelper.java
+++
@@ -1,9 +0,0 @@
-package net.teamsolar.simplest_broadaxes;
-
-import net.neoforged.fml.ModContainer;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-
-public class RegistrationHelper {
-
-}
