5c74e67fb4a5e4f74aef9e08516466e8729575c8
xenonli <126023994+xenonli@users.noreply.github.com>
2025-01-28 13:08:34 -0500

    Update BroadaxeItem.kt
    -60% efficiency modifier -> -80%

--- src/main/kotlin/net/teamsolar/simplest_broadaxes/item/BroadaxeItem.kt
+++ src/main/kotlin/net/teamsolar/simplest_broadaxes/item/BroadaxeItem.kt
@@ -185,7 +185,7 @@ class BroadaxeItem(tier: Tier, properties: Properties) : DiggerItemWithoutDurabi
     val secondaryMineableBlocks: TagKey<Block> = ModTagBlocks.SECONDARY_FELLABLE_BLOCK
 
     companion object {
-        val efficiencyStatModifier = 0.4f
+        val efficiencyStatModifier = 0.2f
         val tickEvent = ModLevelTickEvent()
     }
 }
