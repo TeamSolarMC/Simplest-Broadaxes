691f5a992b004dd4c2560556a44cb4d91fc9dffc
xeli <sheeteditor2@gmail.com>
2025-01-27 19:22:42 -0500

    add broadaxe 3x3 log stripping

---
+++ src/main/kotlin/net/teamsolar/simplest_broadaxes/item/AxeUtils.kt
@@ -0,0 +1,37 @@
+package net.teamsolar.simplest_broadaxes.item
+
+import net.fabricmc.fabric.mixin.content.registry.AxeItemAccessor
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.block.PillarBlock
+import net.minecraft.item.AxeItem
+import net.minecraft.item.ItemUsageContext
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec3d
+import net.minecraft.util.math.Vec3i
+import java.util.*
+
+class AxeUtils {
+    companion object {
+        public fun getStrippedState(state: BlockState): Optional<BlockState> {
+            val STRIPPABLES = AxeItemAccessor.getStrippedBlocks()
+            return Optional.ofNullable(STRIPPABLES[state.block]).map { block: Block ->
+                block.defaultState.with(
+                    PillarBlock.AXIS,
+                    state.get(PillarBlock.AXIS) as Direction.Axis
+                ) as BlockState
+            }
+        }
+
+        public fun ItemUsageContext.offset(offset: Vec3i): ItemUsageContext {
+            val newLocation = hitPos.add(Vec3d.of(offset))
+            val newBlockPos = blockPos.add(offset)
+            return ItemUsageContext(world, player, hand, stack,
+                BlockHitResult(
+                    newLocation, side, newBlockPos, hitsInsideBlock()
+                )
+            )
+        }
+    }
+}
